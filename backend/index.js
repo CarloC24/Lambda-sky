814a85036ed9558e77e3651bed40eeafe95b1589
Blake Fletcher <blkfltchr@gmail.com>
2019-02-20 21:44:32 -0500

    Moved mongodbURI to config file

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,13 @@
 const server = require('./server');
 const port = process.env.PORT || 9000;
 const mongoose = require('mongoose');
+const keys = require('./config/keys');
 
 const passportConfig = require('./config/passport-config');
 
 const users = require('./routes/AuthRoutes')
 
-mongoose.connect('mongodb://carlo:carloc1@ds111622.mlab.com:11622/lambda-sky');
+mongoose.connect(keys.mongodb.dbURI);
 mongoose.connection.once('open', () => {
   console.log('connected to the database');
 });
