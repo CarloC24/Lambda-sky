c075c7664ebfc12db614ea36e6718ac13a9c9243
Blake Fletcher <blkfltchr@gmail.com>
2019-02-21 20:58:02 -0500

    First crack at saving new User in callback

--- backend/config/passport-config.js
+++ backend/config/passport-config.js
@@ -1,13 +1,34 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
 const keys = require('./keys');
+const User = require('../models/Users');
 
-passport.use(new GoogleStrategy({
-  // Google+ API Keys
-  callbackURL: '/auth/google/redirect',
-  clientID: keys.google.clientID,
-  clientSecret: keys.google.clientSecret
+passport.use(
+  new GoogleStrategy({
+      // Google+ API Options
+      callbackURL: '/auth/google/redirect',
+      clientID: keys.google.clientID,
+      clientSecret: keys.google.clientSecret
   }, (accessToken, refreshToken, profile, done) => {
-    console.log(profile);
+      // passport callback function
+      console.log('passport callback function fired:');
+      console.log(profile);
+      new User({
+          googleId: profile.id,
+          firstName: profile.name.givenName,
+          lastName: profile.name.familyName
+      })
+      .save()
+      .then((newUser) => {
+          console.log('new user: ' + newUser);
+      });
   })
-)
+);
+
+// passport.deserializeUser(
+//     User.deserializeUser()
+// )
+
+// passport.serializeUser(
+//     User.serializeUser()
+// )
--- backend/models/Users.js
+++ backend/models/Users.js
@@ -1,34 +1,9 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true
-  },
-  email: {
-    type: String,
-    required: true
-  },
-  password: {
-    type: String,
-    required: true
-  }
+  googleId: String,
+  firstName: String,
+  lastName: String
 });
 
 module.exports = mongoose.model('User', UserSchema);
-
-// const mongoose = require('mongoose');
-// const passportMongoose = require('passport-local-mongoose');
-
-// const UserSchema = new mongoose.Schema({
-//   email: {
-//     type: String
-//   },
-//   name: {
-//     type: String
-//   }
-// });
-
-// UserSchema.plugin(passportMongoose, { usernameField: 'email' });
-
-// module.exports = mongoose.model('User', UserSchema);
