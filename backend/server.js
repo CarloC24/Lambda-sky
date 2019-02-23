adfd0afcfc3debc0c6117fdf19f74dae1d445032
Juan Carlo Clamucha <carloclamucha@Carlo-Macbook.local>
2019-02-22 21:57:35 -0600

    setting up

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,11 @@ const express = require('express');
 const server = express();
 const cookieSession = require('cookie-session');
 const keys = require('./config/keys');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
 const passport = require('passport');
+require('./config')
 
 server.get('/', (req, res) => {
   res.send('Lambda sky ☁️☁️☁️');
@@ -10,10 +14,25 @@ server.get('/', (req, res) => {
 
 // Initialize cookie and passport
 
-server.use(cookieSession({
-  maxAge: 8640000, // 1 day in milliseconds
-  keys: [keys.session.cookieKey]
-}));
+// server.use(cookieSession({
+//   maxAge: 8640000, // 1 day in milliseconds
+//   keys: [keys.session.cookieKey]
+// }));
+// Exposes a bunch of methods for validating data. Used heavily on userController.validateRegister
+app.use(expressValidator());
+
+// populates req.cookies with any cookies that came along with the request
+app.use(cookieParser());
+
+app.use(
+  session({
+    secret: process.env.SECRET,
+    key: process.env.KEY,
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({ mongooseConnection: mongoose.connection })
+  })
+);
 
 server.use(passport.initialize());
 server.use(passport.session());
