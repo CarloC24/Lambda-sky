dd67b56f2270a59bc790d84d7fbe0355b529a4b7
Blake Fletcher <blkfltchr@gmail.com>
2019-02-22 20:06:42 -0500

    Can register users, verified with postman and compass

--- backend/routes/AuthRoutes.js
+++ backend/routes/AuthRoutes.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const passport = require('passport');
+const bcrypt = require('bcryptjs');
+const User = require('../models/Users');
 
 // ROUTE:   GET auth/login
 // DESC:    Tests login route
@@ -28,5 +30,34 @@ router.get('/google', passport.authenticate('google', {
 router.get('/google/redirect', passport.authenticate('google'), (req, res) => {
   res.redirect('/profile');
 });
+
+// ROUTE:   GET auth/register
+// DESC:    Register user
+// ACCESS:  Public
+router.post('/register', (req, res) => {
+  User.findOne({ email: req.body.email })
+    .then(user => {
+      if(user) {
+        return res.status(400).json({email: 'Email already exists'})
+      } else {
+
+         const newUser = new User({
+          name: req.body.name,
+          email: req.body.email,
+          password: req.body.password,
+        });
+
+         bcrypt.genSalt(10, (err, salt) => {
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if(err) throw err;
+            newUser.password = hash;
+            newUser.save()
+              .then(user => res.json(user))
+              .catch(err => console.log(err));
+          })
+        })
+      }
+    })
+})
 
 module.exports = router; 
