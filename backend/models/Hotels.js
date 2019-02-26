43ac6613c43efc3251d1bb863363ac6cd3e08b8f
Nedu Robert <nedumrobert@gmail.com>
2019-02-26 16:29:02 +0100

    Feat: Hotels model

--- backend/models/Hotels.js
+++ backend/models/Hotels.js
@@ -3,7 +3,18 @@ const mongoose = require('mongoose');
 const HotelsSchema = new mongoose.Schema({
   name: {
     type: String
-  }
+  },
+  location: {
+    address: {
+      type: String
+    },
+    city: {
+      type: String
+    },
+    state: {
+      type: String
+    }
+}
 });
 
 module.exports = mongoose.model('Hotels', HotelsSchema);
